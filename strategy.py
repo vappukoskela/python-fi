bc094644eaac469eb6637cb43a8c80b15d747010
Jarkko1966 <j.koskela@windowslive.com>
2025-10-20 20:45:59 +0300

    Fix indentation in TradingClient initialization

--- strategy.py
+++ strategy.py
@@ -206,8 +206,7 @@ def main():
         os.getenv("ALPACA_PAPER_SECRET_KEY")
     )
     trade_client = TradingClient(
-        os
-          trade_client = TradingClient(
+        os trade_client = TradingClient(
         os.getenv("ALPACA_PAPER_API_KEY"),
         os.getenv("ALPACA_PAPER_SECRET_KEY"),
         paper=True
