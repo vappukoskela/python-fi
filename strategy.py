0d08a108d7c0651d662cd315943921ca2790b1e5
Jarkko1966 <j.koskela@windowslive.com>
2025-10-27 22:37:27 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -394,7 +394,7 @@ def main():
                                     if len(vwap_series) >= 3 and not pd.isna(vwap_series.iloc[-1]):
                                          
                                         vwap_fail = all(prices_series.iloc[-i] < vwap_series.iloc[-i] for i in range(1, 4))
-                                        )
+                                        
                             except Exception:
                                 vwap_fail = False
                     
