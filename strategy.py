60bfe1c7e100cdcc5bbbce33e091048507f64bf6
Jarkko1966 <j.koskela@windowslive.com>
2025-11-02 21:09:56 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,10 +1,15 @@
 import requests
+import re
 
 url = "https://raw.githubusercontent.com/vappukoskela/python-fi/jarkkobackup8/strategy.py"
 resp = requests.get(url)
 resp.raise_for_status()
 
 for lineno, line in enumerate(resp.text.splitlines(), start=1):
-    if "entry_times" in line or "last_exit_time" in line:
-        print(f"{lineno:4d}: {line.strip()}")
+    # etsitään kaikki rivit joissa esiintyy idx[0]
+    if re.search(r"idx
 
+\[0\]
+
+", line):
+        print(f"{lineno:4d}: {line.strip()}")
