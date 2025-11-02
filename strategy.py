d17c459614f014cb8f75cdeb12743f6601908ca1
Jarkko1966 <j.koskela@windowslive.com>
2025-11-02 19:55:34 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -6,9 +6,5 @@ jarkkobackup8 = "strategy.py"
 with open(filename, "r", encoding="utf-8") as f:
     for lineno, line in enumerate(f, start=1):
         # etsitään kaikki rivit joissa esiintyy ts
-        if re.search(r"\bts\b", line) or re.search(r"idx
-
-\[0\]
-
-", line):
+        if re.search(r"\bts\b", line) or re.search(r"idx\[0\]", line):
             print(f"{lineno:4d}: {line.strip()}")
