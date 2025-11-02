e123164a0f70f85f07d97bb26975f7734c4d845b
Jarkko1966 <j.koskela@windowslive.com>
2025-11-02 21:11:06 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -7,9 +7,5 @@ resp.raise_for_status()
 
 for lineno, line in enumerate(resp.text.splitlines(), start=1):
     # etsitään kaikki rivit joissa esiintyy idx[0]
-    if re.search(r"idx
-
-\[0\]
-
-", line):
+    if re.search(r"idx\[0\]", line):
         print(f"{lineno:4d}: {line.strip()}")
