3995aa0a09a8c8a4daf689d98476bb93ad215ae5
Jarkko1966 <j.koskela@windowslive.com>
2025-11-02 19:58:39 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -3,7 +3,7 @@ import re
 # vaihda tähän oman tiedostosi nimi
 jarkkobackup8 = "strategy.py"
 
-with open(filename, "r", encoding="utf-8") as f:
+with open(jarkkobackup8, "r", encoding="utf-8") as f:
     for lineno, line in enumerate(f, start=1):
         # etsitään kaikki rivit joissa esiintyy ts
         if re.search(r"\bts\b", line) or re.search(r"idx\[0\]", line):
