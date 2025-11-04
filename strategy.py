02d0cd2cc93d57d0172e98ee2a0f5efa117688f4
Jarkko1966 <j.koskela@windowslive.com>
2025-11-04 20:41:24 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -492,7 +492,7 @@ def main():
         
         symbol = "NVDA"
         start="2024-10-01T14:30:00Z"
-        end="2024-10-01T14:31:00Z"
+        end="2024-10-01T14:45:00Z"
 
         req = StockTradesRequest(
             symbol_or_symbols=symbol,
