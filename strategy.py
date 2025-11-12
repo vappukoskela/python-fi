86ac9b22ad2afa498f77de773279d7c3c6854a6e
Jarkko1966 <j.koskela@windowslive.com>
2025-11-12 17:55:22 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -414,11 +414,7 @@ def evaluate_sell(sym, last_price, ref_entry, price_deque, size_deque, entry_tim
                           sym, entry_time, elapsed)
         
 
-            # --- DEBUG PATCH: logita entry_time ja elapsed ---
-            logging.debug("[%s] DEBUG PATCH | entry_time=%s | elapsed=%.2f seconds",
-                          sym, entry_time, elapsed)
-
-                                
+                                            
         else:
             entry_time = None
             elapsed = 0
