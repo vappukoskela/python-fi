073d9d8a938af5b56f28d3ec6e3199891f0472e2
Jarkko1966 <j.koskela@windowslive.com>
2025-11-22 01:59:00 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1136,8 +1136,8 @@ def main():
         last_exit_time = {s: None for s in symbols}
         last_buy_time = {s: None for s in symbols}
 
-        start = "2025-11-20T14:30:00Z"
-        end = "2025-11-20T21:00:00Z"
+        start = "2025-11-21T14:30:00Z"
+        end = "2025-11-21T21:00:00Z"
     
         req = StockTradesRequest(symbol_or_symbols=symbol, start=start, end=end)
         trades = stock_data_client.get_stock_trades(req).df
