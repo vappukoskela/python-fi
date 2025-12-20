a5ae2ce05a9818bc2e1d317a4212c258449a9cb0
Jarkko1966 <j.koskela@windowslive.com>
2025-12-20 14:54:11 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -2195,7 +2195,6 @@ def main():
                     price_deques[symbol],            # price_deque
                     size_deques[symbol],             # size_deque
                     entry_times,                     # full entry_times dict
-                    entry_configs,
                     entry_configs[symbol],           # CONFIG for this entry
                     current_time=ts_val,             # keyword
                     regime=regime,                   # keyword
