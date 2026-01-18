68fcf719120630f57c400f1f0289a4c1d0f803b0
Jarkko1966 <j.koskela@windowslive.com>
2026-01-18 19:17:59 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -2249,7 +2249,8 @@ def main():
         os.getenv("ALPACA_PAPER_SECRET_KEY"),
         paper=True
     )
-    symbols = ["AAPL", "MSFT", "MU", "QCOM", "NVDA", "V", "AMD", "GOOG", "C", "EBAY", "OKTA", "TSLA", "AMZN", "ADSK", "DELL"]
+    symbols = ["AAPL", "MSFT", "MU", "QCOM", "NVDA", "V", "AMD", "GOOG", "C", "EBAY", "OKTA", "TSLA", "AMZN", "ADSK", "DELL",
+               "SPY","QQQ","IWM","XLK","NFLX","COST","CRM","ORCL"]
     price_deques = {s: deque(maxlen=TICKS_WINDOW) for s in symbols}
     size_deques = {s: deque(maxlen=TICKS_WINDOW) for s in symbols}
     time_deques = {s: deque(maxlen=TICKS_WINDOW) for s in symbols}
