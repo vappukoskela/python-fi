9b565b2d123bd7097437f94831039c6364a3aff2
Jarkko1966 <j.koskela@windowslive.com>
2026-01-29 22:21:09 +0200

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -3533,6 +3533,9 @@ def main():
                         continue
 
                     # --- Execute BUY ---
+                    logging.info("[LOOP_BUY_CALL] calling safe_market_buy for %s", symbol)
+                    logging.debug("[DICT_ID_CALLSITE] entry_times id=%s entry_prices id=%s entry_qty id=%s entry_configs id=%s",
+                    
                     safe_market_buy(
                         trade_client_local=trade_client,
                         symbol=symbol,
